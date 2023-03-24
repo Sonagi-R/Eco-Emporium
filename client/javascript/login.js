6db5e6586b322e7cf9befd2f824defaed5172e8a
MisterCxmpy <cosgunhussein@gmail.com>
2023-03-24 23:59:56 +0000

    minor changes

--- client/javascript/login.js
+++ client/javascript/login.js
@@ -1,11 +1,15 @@
 const signInBtn = document.querySelector("#sign-in-btn")
 const signUpBtn = document.querySelector("#sign-up-btn")
 const formCover = document.querySelector("#form-cover")
+const signInForm = document.querySelector("#sign-in-form")
+const signUpForm = document.querySelector("#sign-up-form")
 
 signUpBtn.addEventListener("click", () => {
     formCover.classList.add("active")
+    signInForm.reset()
 })
 
 signInBtn.addEventListener("click", () => {
     formCover.classList.remove("active")
+    signUpForm.reset()
 })
